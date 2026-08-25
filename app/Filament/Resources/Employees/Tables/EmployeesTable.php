b3544f1f4f87310875419db758fc960ae847c98a
MUCHIRAJUNIOR <muchirajunior022@gmail.com>
2026-08-25 20:06:58 +0300

    Update employees

--- app/Filament/Resources/Employees/Tables/EmployeesTable.php
+++ app/Filament/Resources/Employees/Tables/EmployeesTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Employees\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -56,6 +57,7 @@ class EmployeesTable
             ])
             ->recordActions([
                 EditAction::make(),
+                ViewAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
