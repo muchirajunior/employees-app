8c3f8c8e6a7e16c9f749faaab52077b8aa1717d3
MUCHIRAJUNIOR <muchirajunior022@gmail.com>
2026-08-30 13:00:06 +0300

    Fix slug unique issue

--- app/Filament/Pages/Tenancy/RegisterTeam.php
+++ app/Filament/Pages/Tenancy/RegisterTeam.php
@@ -4,9 +4,11 @@ namespace App\Filament\Pages\Tenancy;
 
 use App\Models\Team;
 use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
 use Filament\Pages\Tenancy\RegisterTenant;
 use Filament\Schemas\Schema;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class RegisterTeam extends RegisterTenant
 {
@@ -26,6 +28,17 @@ class RegisterTeam extends RegisterTenant
 
     protected function handleRegistration(array $data): Team
     {
+        if (Team::where('slug', $data['slug'])->exists()) {
+             Notification::make()
+                ->danger()
+                ->title(__('Registration failed'))
+                ->body(__('This team slug has already been taken.'))
+                ->send();
+            
+            throw ValidationException::withMessages([
+                'slug' => __('This team slug has already been taken.'),
+            ]);
+        }
         $team = Team::create($data);
 
         $team->members()->attach(Auth::user());
